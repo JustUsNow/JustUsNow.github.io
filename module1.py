5d12cad88078e0e12bcdb0cf4a04eb51099ddefe
JustUsNow <79359475+JustUsNow@users.noreply.github.com>
2021-03-31 17:37:25 +0700

    bot_test

---
+++ module1.py
@@ -0,0 +1,26 @@
+from vk_bot import VkBot
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+# API-ключ созданный ранее
+token = "b6fef6432be4df1b92cfc27cb3e88666d00d2229a272fe41a05fba202517575e260c1707539fbe8dbc175"
+
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
+
+# Работа с сообщениями
+longpoll = VkLongPoll(vk)
+
+print("Server started")
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+        
+            print('New message:')
+            print(f'For me by: {event.user_id}', end='')
+            
+            bot = VkBot(event.user_id)
+            write_msg(event.user_id, bot.new_message(event.text))
+            
+            print('Text: ', event.text)
+        
