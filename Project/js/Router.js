d41b0e35b2b04436268caafeb4bae6ba8cde1301
JustUsNow <79359475+JustUsNow@users.noreply.github.com>
2021-11-24 23:04:05 +0700

    Update Router.js

--- Project/js/Router.js
+++ Project/js/Router.js
@@ -29,37 +29,28 @@ Router.prototype = {
         this.hasChanged(this, r);
     },
     hasChanged: function(scope, r) {
-        console.log('hasChanged' + window.location.hash);
         if(window.location.hash.length > 0) {
             console.log(r);
             for (var i = 0, length = r.length; i < length; i++) {
-                console.log(window.location.hash.substr(1));
                 var route = r[i];
                 if(route.isActiveRoute(window.location.hash.substr(1))) {
-                    console.log('1');
                     scope.goToRoute(route.htmlName);
                 }
             }
-        } else {
-            console.log('else');
             for (var i = 0, length = r.length; i < length; i++) {
                 var route =  r[i];
                 if(route.defaultRoute) {
-                    console.log('2');
                     scope.goToRoute(route.htmlName);
                 }
             }
         }
     },
     goToRoute: function(htmlName) {
-        console.log('goToRoute');
         (function(scope) {
-            console.log('request');
             var url = 'pages/' + htmlName, 
                 xhttp = new XMLHttpRequest();
-                console.log(url);
             xhttp.onreadystatechange = function() {
-                console.log(htmlName);
+                console.log(this.readyState, ' ', this.status);
                 if(this.readyState === 4 && this.status === 200) {
                     scope.rootElem.innerHTML = this.responceText;
                 }
