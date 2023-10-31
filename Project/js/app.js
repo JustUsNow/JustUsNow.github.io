eacb1a108727356f7ee07a1eb20fca78d87e7cb7
JustUsNow <79359475+JustUsNow@users.noreply.github.com>
2023-10-31 11:04:53 +0700

    Update app.js

--- Project/js/app.js
+++ Project/js/app.js
@@ -6,7 +6,10 @@
             new Route('home', 'home.html', true),
             new Route('about', 'about.html'),
             new Route('intro', 'intro.html'),
-            new Route('matrix', 'matrix.html')
+            new Route('matrix', 'matrix.html'),
+            new Route('behaviours', 'behaviours.html'),
+            new Route('burn', 'burn.html'),
+            new Route('conclusion', 'conclusion.html')
         ]);
     }
     init();
